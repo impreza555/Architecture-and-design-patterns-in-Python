a517bf51a1d0d35f15945262de1ec7109f185cfe
impreza555 <alex.erjomin@outlook.com>
2022-05-16 13:19:11 +0300

    Сделал Д.З.1.

--- my_project/run.py
+++ my_project/run.py
@@ -8,4 +8,5 @@ application = Framework(routes, fronts)
 
 with make_server('', 8080, application) as httpd:
     print("Запуск на порту 8080...")
+    print('Поехали http://localhost:8080/')
     httpd.serve_forever()
