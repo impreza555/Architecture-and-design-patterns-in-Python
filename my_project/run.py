3bf6af464c34c3e056fa8ac0dc9309ebc17f39d0
impreza555 <alex.erjomin@outlook.com>
2022-06-03 17:02:04 +0300

    Подтянул статику по феншую.

--- my_project/run.py
+++ my_project/run.py
@@ -1,10 +1,12 @@
 from wsgiref.simple_server import make_server
+from whitenoise import WhiteNoise
 
 from geek_framework.main import Framework
 from urls import fronts
 from views import routes
 
 application = Framework(routes, fronts)
+application = WhiteNoise(application, root="./static/")
 
 with make_server('', 8080, application) as httpd:
     print("Запуск на порту 8080...")
